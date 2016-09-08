bc5693c2ba552cceebc9f9006492ea132a5479f7
LaMachineCaron <Alexandre_Caron@outlook.com>
2016-09-08 10:19:11 -0400

    Finish-ish InGame activity, waiting on the NFC part.

--- app/src/main/java/com/example/res260/NFCProject/MainActivity.java
+++ app/src/main/java/com/example/res260/NFCProject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.res260.NFCProject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, InGame.class);
+        startActivity(intent);
+        //setContentView(R.layout.activity_main);
     }
 }
